a81e29217accff70b3b914a6430f57f3eebd6239
vilasack khamchaleun <Kola@Kolas-MacBook-Pro.local>
2017-09-01 18:10:20 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/canberra/ucstudyabroadapp/Log_in_Slide.java
@@ -0,0 +1,13 @@
+package au.edu.canberra.ucstudyabroadapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Log_in_Slide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in__slide);
+    }
+}
